46b75b0303c05ce72110e9fb12d90fe5bdaac160
David M. Carr <david@carrclan.us>
2024-03-08 18:48:03 -0500

    Add uninstall script

---
+++ local-uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+addonName="VarzofTweaks"
+addonsDir="/Applications/World of Warcraft/_retail_/Interface/AddOns"
+addonDir="${addonsDir}/${addonName}"
+rm -rf "$addonDir"
